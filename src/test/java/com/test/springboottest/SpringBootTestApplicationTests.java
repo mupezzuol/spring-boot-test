9d095aaf606d50977220d4784fdba3be2c148dd8
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-08-26 11:41:28 +0100

    upload project

---
+++ src/test/java/com/test/springboottest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
