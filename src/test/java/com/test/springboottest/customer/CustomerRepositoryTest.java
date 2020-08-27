ad3fdda85ff4fb6b4aacb7ff0dc2912be2f75c94
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-08-27 11:35:00 +0100

    Add more test on CustomerRepository

--- src/test/java/com/test/springboottest/customer/CustomerRepositoryTest.java
+++ src/test/java/com/test/springboottest/customer/CustomerRepositoryTest.java
@@ -5,21 +5,49 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@DataJpaTest
+// For valid fields in Entity
+@DataJpaTest(
+        properties = {"spring.jpa.properties.javax.persistence.validation.mode=none"}
+)
 class CustomerRepositoryTest {
 
     @Autowired
     private CustomerRepository underTest;
 
     @Test
-    void itShouldSelectCustomerByPhoneNumber() {
+    void itShouldSelectCustomerByPhoneNumberExists() {
         // Given
+        UUID id = UUID.randomUUID();
+        String phoneNumber = "0000";
+        Customer customer = new Customer(id, "Murillo", phoneNumber);
+
         // When
+        underTest.save(customer);
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(phoneNumber);
+
         // Then
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+                    assertThat(c).isEqualToComparingFieldByField(customer);
+                });
+    }
+
+    @Test
+    void itNotShouldSelectCustomerByPhoneNumberDoesNotExists() {
+        // Given
+        String phoneNumber = "0000";
+
+        // When
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(phoneNumber);
+
+        // Then
+        assertThat(optionalCustomer).isNotPresent();
     }
 
     @Test
@@ -38,5 +66,31 @@ class CustomerRepositoryTest {
                 .hasValueSatisfying(c -> {
                     assertThat(c).isEqualToComparingFieldByField(customer);
                 });
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenNameIsNull() {
+        // Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, null, "0000");
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.save(customer))
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessageContaining("not-null property references a null or transient value : com.test.springboottest.customer.Customer.name");
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenPhoneNumberIsNull() {
+        // Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Murillo", null);
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.save(customer))
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessageContaining("not-null property references a null or transient value : com.test.springboottest.customer.Customer.phoneNumber");
     }
 }
