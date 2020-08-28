01fad54109266ef3bbedc17f003d13c3694d9a40
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-08-28 15:30:43 +0100

    using ParameterizedTest and CsvSource

--- src/test/java/com/test/springboottest/utils/PhoneNumberValidatorTest.java
+++ src/test/java/com/test/springboottest/utils/PhoneNumberValidatorTest.java
@@ -3,6 +3,8 @@ package com.test.springboottest.utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,6 +15,23 @@ public class PhoneNumberValidatorTest {
     @BeforeEach
     void setUp(){
         underTest = new PhoneNumberValidator();
+    }
+
+    //In this way, we passed several parameters and received the method for running the test.
+    @DisplayName("It should validate list of phoneNumber")
+    @ParameterizedTest
+    @CsvSource({
+            "+447000000000,true",
+            "+4470000000003,false",
+            "447000000000,false",
+            "null,false"
+    })
+    void itShouldValidatePhoneNumber(String phoneNumber, boolean expected) {
+        // When
+        boolean isValid = underTest.test(phoneNumber);
+
+        // Then
+        assertThat(isValid).isEqualTo(expected);
     }
 
     @Test
